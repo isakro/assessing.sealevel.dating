a720c123151c427943986ae9238db7479e42a8b9
isakro <isak.hkr@hotmail.com>
2021-08-18 15:13:34 +0200

    Adding displacement curve for Skoppum and revised version of Gunnarsrød

--- analysis/script/02script.R
+++ analysis/script/02script.R
@@ -2,5 +2,10 @@ library(tidyverse)
 library(tmap)
 
 # Load radiocarbon and site data from the first script
-load(here::here("analysis/data/derived_data/data.RData"))
+# load(here::here("analysis/data/derived_data/data.RData"))
 
+# Read in displacement curves
+arendal <- read.csv("bjornebu.csv")
+tvedestrand <- read.csv("hanto.csv")
+larvik <- read.csv("gunnarsrød.csv")
+skoppum
